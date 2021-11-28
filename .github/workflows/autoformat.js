706fac6484b0c7761b01018ad6708fe23520127a
harupy <hkawamura0130@gmail.com>
2021-11-28 18:23:23 +0900

    a

--- .github/workflows/autoformat.js
+++ .github/workflows/autoformat.js
@@ -5,57 +5,44 @@ const getData = async (context, github) => {
   const pr = await github.pulls.get({ owner, repo, pull_number });
   const { sha } = pr.data.head;
   const run_url = `https://github.com/${owner}/${repo}/actions/runs/${runId}`;
-  const pr_url = `https://github.com/${owner}/${repo}/pull/${pull_number}`;
   return {
     owner,
     repo,
     sha,
     workflow,
     run_url,
-    pr_url,
   };
 };
 
-const objectToMarkdownTable = obj => {
-  return [
-    "|Key|Value|",
-    "|---|-----|",
-    ...Object.entries(obj).map(([key, value]) => `|${key}|${value}|`),
-  ].join("\n");
-};
-
-const createCheckRun = async (context, github) => {
-  const { owner, repo, sha, run_url } = await getData(context, github);
-  const state = "pending";
-  const check_run = await github.repos.createCommitStatus({
-    owner,
-    repo,
-    sha,
-    state,
-    target_url: run_url,
-    context: "autoformat",
-  });
-  console.log(check_run.data);
-  return check_run.data.id;
-};
-
-const updateCheckRun = async (context, github, needs) => {
-  const { owner, repo, sha, run_url } = await getData(context, github);
-  const failed = Object.values(needs).some(
-    ({ result }) => result === "failure"
+const createCommitStatus = async (context, github, state) => {
+  const { owner, repo, sha, run_url, workflow } = await getData(
+    context,
+    github
   );
-  const state = failed ? "failure" : "success";
   await github.repos.createCommitStatus({
     owner,
     repo,
     sha,
     state,
+    description: state,
     target_url: run_url,
-    context: "autoformat",
+    context: workflow,
   });
 };
 
+const createStatus = async (context, github) => {
+  await createCommitStatus(context, github, "pending");
+};
+
+const updateStatus = async (context, github, needs) => {
+  const failed = Object.values(needs).some(
+    ({ result }) => result === "failure"
+  );
+  const state = failed ? "failure" : "success";
+  await createCommitStatus(context, github, state);
+};
+
 module.exports = {
-  createCheckRun,
-  updateCheckRun,
+  createStatus,
+  updateStatus,
 };
